bfd77a325f2c9830a6d26f435de48f1c80c74f2d
yektas <ahmetsercanyektas@gmail.com>
2022-02-09 01:37:43 +0000

    Deploying to gh-pages from @ yektas/heroic-editor@788b31e30ca50731041df07ed1e28311a56f1b51 🚀

---
+++ _next/static/GgFB8HDqMMtlvYgq9EoNb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1250-811c26599b7aeef8.js","static/chunks/pages/index-81770ebd6678c7f4.js"],"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
