9a504bae628359b6811684fd643f88875ba61e89
yektas <ahmetsercanyektas@gmail.com>
2022-02-09 04:27:21 +0300

    replace variables with env variables

--- context/socketContext.tsx
+++ context/socketContext.tsx
@@ -1,7 +1,9 @@
 import { createContext, useContext, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
-const socket = io(process.env.NEXT_PUBLIC_BASE_URL!);
+const socket = io(
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000"
+);
 
 interface Context {
   socket: Socket;
--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? "/heroic-editor" : "/",
-  assetPrefix: isProd ? "/heroic-editor/" : "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
