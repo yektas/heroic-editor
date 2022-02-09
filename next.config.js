f7f15e0eeea50c67f8a047b9eaf0854b11060340
yektas <ahmetsercanyektas@gmail.com>
2022-02-09 04:19:20 +0300

    fix fonts

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/heroic-editor",
+  basePath: isProd ? "/heroic-editor" : "/",
   assetPrefix: isProd ? "/heroic-editor/" : "",
 };
 
