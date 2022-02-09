233e2b8326a3528b914622c4e2aa900a453348b8
yektas <ahmetsercanyektas@gmail.com>
2022-02-09 04:14:46 +0300

    update deploy script

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/heroic-editor",
+  assetPrefix: isProd ? "/heroic-editor/" : "",
 };
 
 module.exports = nextConfig;
